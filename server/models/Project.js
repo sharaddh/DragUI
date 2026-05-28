ec9e8be295347ff62a209253f251dd039f4ee7db
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-05-28 12:18:19 +0530

    user componet dir

--- server/models/Project.js
+++ server/models/Project.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const projectSchema = new mongoose.Schema({
   userId: String,
   name: String,
+  uniqueId: { type: String, index: true },
+  isPublic: { type: Boolean, default: false },
   design: Object, // your tree JSON
   updatedAt: {
     type: Date,
