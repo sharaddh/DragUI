cd5c74fa49258515cc688615c191d2f2bd5bc222
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-05-30 20:44:38 +0530

    shartcut cammands

--- Client/src/components/Shortcuts.jsx
+++ Client/src/components/Shortcuts.jsx
@@ -1,27 +1,27 @@
-import { useEffect } from "react";
-import { useBuilderStore } from "../store/useBuilderStore";
+// import { useEffect } from "react";
+// import { useBuilderStore } from "../store/useBuilderStore";
 
-export default function Shortcuts() {
-  const { undo, redo } = useBuilderStore();
+// export default function Shortcuts() {
+//   const { undo, redo } = useBuilderStore();
 
-  useEffect(() => {
-    const handler = (e) => {
-      // Undo → Ctrl + Z
-      if (e.ctrlKey && e.key === "z") {
-        e.preventDefault();
-        undo();
-      }
+//   useEffect(() => {
+//     const handler = (e) => {
+//       // Undo → Ctrl + Z
+//       if (e.ctrlKey && e.key === "z") {
+//         e.preventDefault();
+//         undo();
+//       }
 
-      // Redo → Ctrl + Y
-      if (e.ctrlKey && e.key === "y") {
-        e.preventDefault();
-        redo();
-      }
-    };
+//       // Redo → Ctrl + Y
+//       if (e.ctrlKey && e.key === "y") {
+//         e.preventDefault();
+//         redo();
+//       }
+//     };
 
-    window.addEventListener("keydown", handler);
-    return () => window.removeEventListener("keydown", handler);
-  }, [undo, redo]);
+//     window.addEventListener("keydown", handler);
+//     return () => window.removeEventListener("keydown", handler);
+//   }, [undo, redo]);
 
-  return null;
-}
+//   return null;
+// }
