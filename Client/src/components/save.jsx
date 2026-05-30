74006a2694dba9b2c09783ead0476b5c2d84607d
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-05-30 20:48:22 +0530

    rendrer

--- Client/src/components/save.jsx
+++ Client/src/components/save.jsx
@@ -1,4 +1,4 @@
-// This file is deprecated. Code is no longer stored here or in the database.
-// Only props and template references are saved.
+This file is deprecated. Code is no longer stored here or in the database.
+Only props and template references are saved.
 
-// If you need to generate code, use the design tree and template references.
+If you need to generate code, use the design tree and template references.
