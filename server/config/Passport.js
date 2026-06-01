a62ba626c3394153c4ff814c19fc20ee3f70b613
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-01 23:28:42 +0530

    server config- DB, pasport, cloudinary

--- server/config/Passport.js
+++ server/config/Passport.js
@@ -1,52 +1,129 @@
-import dotenv from "dotenv";
-dotenv.config();
 import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import { Strategy as GitHubStrategy } from "passport-github2";
-import User from "../models/user.js";
-import jwt from "jsonwebtoken"; 
 
-// 🔥 GOOGLE
+import { Strategy as GoogleStrategy }
+from "passport-google-oauth20";
+
+import { Strategy as GithubStrategy }
+from "passport-github2";
+
+import User from "../models/User.js";
+
+passport.serializeUser(
+  (user, done) => {
+    done(null, user.id);
+  }
+);
+
+passport.deserializeUser(
+  async (id, done) => {
+    try {
+      const user =
+        await User.findById(id);
+
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  }
+);
+
+// GOOGLE
+
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/auth/google/callback",
+      clientID:
+        process.env.GOOGLE_CLIENT_ID,
+
+      clientSecret:
+        process.env.GOOGLE_CLIENT_SECRET,
+
+      callbackURL:
+        "/api/auth/google/callback",
     },
-    async (_, __, profile, done) => {
-      let user = await User.findOne({ email: profile.emails[0].value });
 
-      if (!user) {
-        user = await User.create({
-          email: profile.emails[0].value,
-          username: profile.id,
-        });
+    async (
+      accessToken,
+      refreshToken,
+      profile,
+      done
+    ) => {
+      try {
+        let user =
+          await User.findOne({
+            googleId: profile.id,
+          });
+
+        if (!user) {
+          user = await User.create({
+            googleId: profile.id,
+
+            email:
+              profile.emails?.[0]?.value,
+
+            username:
+              profile.displayName,
+
+            avatar:
+              profile.photos?.[0]?.value,
+
+            provider: "google",
+          });
+        }
+
+        done(null, user);
+      } catch (error) {
+        done(error);
       }
-
-      return done(null, user);
     }
   )
 );
 
-// 🔥 GITHUB
+// GITHUB
+
 passport.use(
-  new GitHubStrategy(
+  new GithubStrategy(
     {
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "/api/auth/github/callback",
+      clientID:
+        process.env.GITHUB_CLIENT_ID,
+
+      clientSecret:
+        process.env.GITHUB_CLIENT_SECRET,
+
+      callbackURL:
+        "/api/auth/github/callback",
     },
-    async (_, __, profile, done) => {
-      let user = await User.findOne({ username: profile.username });
 
-      if (!user) {
-        user = await User.create({
-          username: profile.username,
-        });
+    async (
+      accessToken,
+      refreshToken,
+      profile,
+      done
+    ) => {
+      try {
+        let user =
+          await User.findOne({
+            githubId: profile.id,
+          });
+
+        if (!user) {
+          user = await User.create({
+            githubId: profile.id,
+
+            username:
+              profile.username,
+
+            avatar:
+              profile.photos?.[0]?.value,
+
+            provider: "github",
+          });
+        }
+
+        done(null, user);
+      } catch (error) {
+        done(error);
       }
-
-      return done(null, user);
     }
   )
 );
