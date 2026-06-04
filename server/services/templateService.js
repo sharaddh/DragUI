121e91fb912ce22b286c30b25e8eed28e904c7d7
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-05 00:00:07 +0530

    compoent servises phase 8 part 5

--- server/services/templateService.js
+++ server/services/templateService.js
@@ -68,4 +68,109 @@ export const saveTemplate =
 
       filePath
     };
+  };import fs from "fs-extra";
+import path from "path";
+
+export const saveTemplate =
+  async ({
+    type,
+    slug,
+    code
+  }) => {
+    const dir = path.join(
+      process.cwd(),
+      "templates",
+      type,
+      slug
+    );
+
+    await fs.ensureDir(dir);
+
+    const filePath =
+      path.join(
+        dir,
+        `${slug}.jsx`
+      );
+
+    await fs.writeFile(
+      filePath,
+      code
+    );
+
+    return {
+      path:
+        `${type}/${slug}`,
+
+      filePath
+    };
+  };import fs from "fs-extra";
+import path from "path";
+
+export const saveTemplate =
+  async ({
+    type,
+    slug,
+    code
+  }) => {
+    const dir = path.join(
+      process.cwd(),
+      "templates",
+      type,
+      slug
+    );
+
+    await fs.ensureDir(dir);
+
+    const filePath =
+      path.join(
+        dir,
+        `${slug}.jsx`
+      );
+
+    await fs.writeFile(
+      filePath,
+      code
+    );
+
+    return {
+      path:
+        `${type}/${slug}`,
+
+      filePath
+    };
+  };import fs from "fs-extra";
+import path from "path";
+
+export const saveTemplate =
+  async ({
+    type,
+    slug,
+    code
+  }) => {
+    const dir = path.join(
+      process.cwd(),
+      "templates",
+      type,
+      slug
+    );
+
+    await fs.ensureDir(dir);
+
+    const filePath =
+      path.join(
+        dir,
+        `${slug}.jsx`
+      );
+
+    await fs.writeFile(
+      filePath,
+      code
+    );
+
+    return {
+      path:
+        `${type}/${slug}`,
+
+      filePath
+    };
   };
