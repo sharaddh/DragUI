176846784f948a9a2ac685c482910a9ce2508948
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-07 23:49:01 +0530

    pahse 9 part 1 context and layout

--- Client/src/components/save.jsx
+++ Client/src/components/save.jsx
@@ -1,4 +1,13 @@
 // This file is deprecated. Code is no longer stored here or in the database.
 // Only props and template references are saved.
 
+// If you need to generate code, use the design tree and template references.// This file is deprecated. Code is no longer stored here or in the database.
+// Only props and template references are saved.
+
+// If you need to generate code, use the design tree and template references.// This file is deprecated. Code is no longer stored here or in the database.
+// Only props and template references are saved.
+
+// If you need to generate code, use the design tree and template references.// This file is deprecated. Code is no longer stored here or in the database.
+// Only props and template references are saved.
+
 // If you need to generate code, use the design tree and template references.
