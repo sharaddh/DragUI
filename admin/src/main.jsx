c825fba68581b5cc0d498ca4d1d196af60f038a2
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-07 18:43:52 +0530

    pahse 9 part 1 context and layout

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import {
+  BrowserRouter,
+} from "react-router-dom";
+
+import {
+  AuthProvider,
+} from "./context/AuthContext";
+
+import App from "./App";
+
+import {
+  Toaster,
+} from "react-hot-toast";
+
+import "./index.css";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+
+  <BrowserRouter>
+
+    <AuthProvider>
+
+      <Toaster />
+
+      <App />
+
+    </AuthProvider>
+
+  </BrowserRouter>
+
+);
