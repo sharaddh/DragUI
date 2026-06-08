8959f7ffd641e4f328c9cf1b8350f364a799abc8
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-08 23:10:42 +0530

    pahse 9 part 2 code editor

---
+++ admin/src/components/ComponentCard.jsx
@@ -0,0 +1,110 @@
+import {
+  Pencil,
+  Trash2,
+  Rocket,
+  Archive
+} from "lucide-react";
+
+import {
+  useNavigate
+} from "react-router-dom";
+
+export default function ComponentCard({
+  component,
+  onDelete,
+  onPublish,
+  onArchive
+}) {
+
+  const navigate =
+    useNavigate();
+
+  return (
+
+    <div
+      className="
+      bg-white
+      border
+      rounded-2xl
+      p-5
+      flex
+      justify-between
+      items-center
+      "
+    >
+
+      <div>
+
+        <h3
+          className="
+          text-lg
+          font-semibold
+          "
+        >
+          {component.name}
+        </h3>
+
+        <p
+          className="
+          text-zinc-500
+          "
+        >
+          {component.category}
+        </p>
+
+      </div>
+
+      <div
+        className="
+        flex
+        gap-3
+        "
+      >
+
+        <button
+          onClick={() =>
+            navigate(
+              `/components/edit/${component._id}`
+            )
+          }
+        >
+          <Pencil size={18}/>
+        </button>
+
+        <button
+          onClick={() =>
+            onPublish(
+              component._id
+            )
+          }
+        >
+          <Rocket size={18}/>
+        </button>
+
+        <button
+          onClick={() =>
+            onArchive(
+              component._id
+            )
+          }
+        >
+          <Archive size={18}/>
+        </button>
+
+        <button
+          onClick={() =>
+            onDelete(
+              component._id
+            )
+          }
+        >
+          <Trash2 size={18}/>
+        </button>
+
+      </div>
+
+    </div>
+
+  );
+
+}
