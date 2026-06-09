41b87764c230e2a7fe82695f674ef22b7d6122e6
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-09 17:39:46 +0530

    Admin  ↓ Create Component  ↓ Version  ↓ Publish  ↓ Marketplace  ↓ CLI Pull

---
+++ admin/src/pages/Marketplace.jsx
@@ -0,0 +1,22 @@
+export default function Marketplace(){
+
+ return(
+
+ <div>
+
+  <h1
+   className="
+   text-3xl
+   font-bold
+   "
+  >
+
+   Marketplace
+
+  </h1>
+
+ </div>
+
+ );
+
+}
--- admin/src/routes/AppRoutes.jsx
+++ admin/src/routes/AppRoutes.jsx
@@ -11,6 +11,8 @@ import ProtectedRoute from "../middleware/ProtectedRoute";
 
 import AdminLayout from "../layouts/AdminLayout";
 import Components from "../pages/Components";
+import Marketplace
+  from "../pages/Marketplace";
 import ComponentEditor from "../pages/ComponentEditor";
 export default function AppRoutes() {
 
@@ -52,7 +54,7 @@ export default function AppRoutes() {
           </ProtectedRoute>
         }
       />
-      
+
       <Route
         path="/components/new"
         element={
@@ -62,13 +64,31 @@ export default function AppRoutes() {
         }
       />
 
-    
-    <Route
-    path="/components/edit/:id"
-    element={
-      <ProtectedRoute>
-            <ComponentEditor/>
-      </ProtectedRoute>
+      <Route
+
+        path="/marketplace"
+
+        element={
+
+          <ProtectedRoute>
+
+            <AdminLayout>
+
+              <Marketplace />
+
+            </AdminLayout>
+
+          </ProtectedRoute>
+
+        }
+
+      />
+      <Route
+        path="/components/edit/:id"
+        element={
+          <ProtectedRoute>
+            <ComponentEditor />
+          </ProtectedRoute>
         }
       />
     </Routes>
