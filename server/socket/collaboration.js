3c114e36855b630f92fba10ea845048a101486d1
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-10 23:05:07 +0530

    ai model client side integration

---
+++ server/socket/collaboration.js
@@ -0,0 +1,35 @@
+export default function registerCollaboration(
+ io,
+ socket
+){
+
+ socket.on(
+  "editor:join",
+
+  ({
+   componentId
+  })=>{
+
+   socket.join(
+    componentId
+   );
+
+  }
+ );
+
+ socket.on(
+  "editor:update",
+
+  payload=>{
+
+   socket.to(
+    payload.componentId
+   ).emit(
+    "editor:update",
+    payload
+   );
+
+  }
+ );
+
+}
