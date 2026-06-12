1856217a4612b0da7d59c42c2124745e7688ed7e
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-12 23:44:05 +0530

    private

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -4,13 +4,13 @@ from "cloudinary";
 cloudinary.config({
 
  cloud_name:
-  process.env.CLOUDINARY_CLOUD_NAME,
+  process.env.CLOUDINARY_CLOUD_NAME || "",
 
  api_key:
-  process.env.CLOUDINARY_API_KEY,
+  process.env.CLOUDINARY_API_KEY || "",
 
  api_secret:
-  process.env.CLOUDINARY_API_SECRET,
+  process.env.CLOUDINARY_API_SECRET || "",
 
 });
 
