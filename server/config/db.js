ff3d26f647195af3307877960b4bb1e274d7afb2
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-12 23:44:23 +0530

    private

--- server/config/db.js
+++ server/config/db.js
@@ -3,18 +3,18 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGO_URI,
+      process.env.MONGO_URI || "mongodb://localhost:27017/dragui",
       {
         autoIndex: true,
       }
     );
 
     console.log(
-      `✅ MongoDB Connected: ${conn.connection.host}`
+      `MongoDB Connected: ${conn.connection.host}`
     );
   } catch (error) {
     console.error(
-      "❌ MongoDB Connection Error:",
+      "MongoDB Connection Error:",
       error.message
     );
 
