010b6f0c9c829f161bbca089e51496c271ee25a7
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-12 22:55:17 +0530

    client

---
+++ admin/src/lib/queryClient.js
@@ -0,0 +1,24 @@
+import {
+ QueryClient
+}
+from "@tanstack/react-query";
+
+export const queryClient =
+new QueryClient({
+
+ defaultOptions:{
+
+  queries:{
+   staleTime:
+    1000 * 60 * 5,
+
+   retry:2,
+
+   refetchOnWindowFocus:
+    false
+
+  }
+
+ }
+
+});
