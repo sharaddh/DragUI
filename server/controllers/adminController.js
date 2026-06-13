e681080b2145be09096fced24e1cc443038e4529
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-13 23:55:56 +0530

    private

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -40,3 +40,86 @@ async (
   }
 
 };
+import Component from "../models/Component.js";
+import Project from "../models/Project.js";
+import User from "../models/User.js";
+
+export const dashboard =
+async (
+  req,
+  res
+) => {
+
+  try {
+
+    const [
+      users,
+      projects,
+      components,
+    ] = await Promise.all([
+      User.countDocuments(),
+      Project.countDocuments(),
+      Component.countDocuments(),
+    ]);
+
+    res.json({
+      success: true,
+
+      stats: {
+        users,
+        projects,
+        components,
+      },
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+
+  }
+
+};import Component from "../models/Component.js";
+import Project from "../models/Project.js";
+import User from "../models/User.js";
+
+export const dashboard =
+async (
+  req,
+  res
+) => {
+
+  try {
+
+    const [
+      users,
+      projects,
+      components,
+    ] = await Promise.all([
+      User.countDocuments(),
+      Project.countDocuments(),
+      Component.countDocuments(),
+    ]);
+
+    res.json({
+      success: true,
+
+      stats: {
+        users,
+        projects,
+        components,
+      },
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+
+  }
+
+};
