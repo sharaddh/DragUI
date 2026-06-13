d3d20d5b044bea1af7a7e0f2d3a8e0627b6e2397
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-13 23:57:52 +0530

    controllers

--- Client/src/GeneratedUI.jsx
+++ Client/src/GeneratedUI.jsx
@@ -4,7 +4,7 @@ import {  } from "DropUi";
 export default function GeneratedUI() {
   return (
     <>
-      <div className="p-4 min-h-screen">
+      <div className="p-4 bg-gray-200 min-h-screen">
 
 </div>
     </>
