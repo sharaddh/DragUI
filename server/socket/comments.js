f22e7208bcd1fd907005cc32dd304d07e61ebf97
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-16 23:45:44 +0530

    cammnd line inteface

---
+++ server/socket/comments.js
@@ -0,0 +1,94 @@
+import { Server } from "socket.io";
+
+import registerBuilderEvents
+from "./builderEvents.js";
+
+import {registerPresence}
+from "./presence.js";
+
+let io;
+
+export const initializeSocket =
+(server) => {
+
+  io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: [
+        "GET",
+        "POST"
+      ]
+    }
+  });
+
+  io.on(
+    "connection",
+    (socket) => {
+
+      console.log(
+        "Socket Connected:",
+        socket.id
+      );
+
+      registerBuilderEvents(
+        io,
+        socket
+      );
+
+      registerPresence(
+        io,
+        socket
+      );
+
+      socket.on(
+        "disconnect",
+        () => {
+          console.log(
+            "Socket Disconnected:",
+            socket.id
+          );
+        }
+      );
+    }
+  );
+
+  return io;
+};
+
+export const getIO =
+() => io;
+
+import registerCollaboration
+from "./collaboration.js";
+
+
+import registerComments
+from "./comments.js";
+
+export default function initSocket(
+ io
+){
+
+ io.on(
+  "connection",
+  socket=>{
+
+   registerCollaboration(
+    io,
+    socket
+   );
+
+   registerPresence(
+    io,
+    socket
+   );
+
+   registerComments(
+    io,
+    socket
+   );
+
+  }
+ );
+
+}
