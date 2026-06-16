5f5d9f22bdf8321177d519c9c6cc00be860260d1
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-16 22:43:58 +0530

    cammnd line inteface

--- cli/bin/dropui.js
+++ cli/bin/dropui.js
@@ -4,44 +4,53 @@ import initCommand from "../commands/init.js";
 import addCommand from "../commands/add.js";
 import searchCommand from "../commands/search.js";
 import listCommand
-from "../commands/list.js";
+    from "../commands/list.js";
 import removeCommand
-from "../commands/remove.js";
+    from "../commands/remove.js";
+import updateCommand
+    from "../commands/update.js";
 const program = new Command();
 
 program
- .name("dropui")
- .description("DropUI CLI")
- .version("1.0.0");
-
- program
- .command("list")
- .description(
-  "List installed components"
- )
- .action(
-  listCommand
- );
-program
- .command(
-  "remove <component>"
- )
- .action(
-  removeCommand
- );
-program
- .command("init")
- .description("Initialize DropUI")
- .action(initCommand);
+    .name("dropui")
+    .description("DropUI CLI")
+    .version("1.0.0");
 
 program
- .command("add <component>")
- .description("Install component")
- .action(addCommand);
+    .command("list")
+    .description(
+        "List installed components"
+    )
+    .action(
+        listCommand
+    );
+program
+    .command(
+        "remove <component>"
+    )
+    .action(
+        removeCommand
+    );
+program
+    .command("init")
+    .description("Initialize DropUI")
+    .action(initCommand);
 
 program
- .command("search <query>")
- .description("Search components")
- .action(searchCommand);
+    .command("add <component>")
+    .description("Install component")
+    .action(addCommand);
 
+program
+    .command("search <query>")
+    .description("Search components")
+    .action(searchCommand);
+
+program
+    .command(
+        "update <component>"
+    )
+    .action(
+        updateCommand
+    );
 program.parse();
