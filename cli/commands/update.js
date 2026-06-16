78a22cbd676a15cff56479bca721228af9ff5242
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-16 22:43:24 +0530

    cammnd line inteface

--- cli/commands/update.js
+++ cli/commands/update.js
@@ -1,18 +1,115 @@
-import add
-from "./add.js";
+import path from "path";
+
+import ora from "ora";
+
+import {
+ getConfig
+}
+from "../utils/config.js";
+
+import {
+ readLockfile,
+ addComponent
+}
+from "../utils/lockfile.js";
+
+import backupFile
+from "../utils/backup.js";
+
+import writeFiles
+from "../utils/fileWriter.js";
+
+import {
+ getManifest,
+ getLatestVersion
+}
+from "../services/registry.js";
 
 export default async function update(
-
  component
-
 ){
 
- await add(
-  component
- );
+ const spinner =
+  ora(
+   "Checking updates..."
+  ).start();
 
- console.log(
-  "Updated"
- );
+ try{
+
+  const lock =
+   readLockfile();
+
+  const installed =
+   lock.components[
+    component
+   ];
+
+  const latest =
+   await getLatestVersion(
+    component
+   );
+
+  if(
+   installed ===
+   latest.version
+  ){
+
+   spinner.succeed(
+    "Already up to date"
+   );
+
+   return;
+  }
+
+  const config =
+   getConfig();
+
+  backupFile(
+
+   path.join(
+
+    config.componentsDir,
+
+    `${component}.jsx`
+
+   )
+
+  );
+
+  const manifest =
+   await getManifest(
+    component
+   );
+
+  await writeFiles(
+
+   manifest.manifest.files,
+
+   config.componentsDir
+
+  );
+
+  addComponent(
+
+   component,
+
+   latest.version
+
+  );
+
+  spinner.succeed(
+
+   `${component}
+    updated`
+
+  );
+
+ }catch(error){
+
+  spinner.fail(
+   error.message
+  );
+
+ }
 
 }
