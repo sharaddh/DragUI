6e83192e33c35315cec347a2a8772a0406ed3af1
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-16 23:46:09 +0530

    cammnd line inteface

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,79 +1,48 @@
-import { Server } from "socket.io";
+import { Server }
+from "socket.io";
 
 import registerBuilderEvents
 from "./builderEvents.js";
 
-import {registerPresence}
+import {
+ registerPresence
+}
 from "./presence.js";
-
-let io;
-
-export const initializeSocket =
-(server) => {
-
-  io = new Server(server, {
-    cors: {
-      origin: "*",
-      methods: [
-        "GET",
-        "POST"
-      ]
-    }
-  });
-
-  io.on(
-    "connection",
-    (socket) => {
-
-      console.log(
-        "Socket Connected:",
-        socket.id
-      );
-
-      registerBuilderEvents(
-        io,
-        socket
-      );
-
-      registerPresence(
-        io,
-        socket
-      );
-
-      socket.on(
-        "disconnect",
-        () => {
-          console.log(
-            "Socket Disconnected:",
-            socket.id
-          );
-        }
-      );
-    }
-  );
-
-  return io;
-};
-
-export const getIO =
-() => io;
 
 import registerCollaboration
 from "./collaboration.js";
 
-
 import registerComments
 from "./comments.js";
 
-export default function initSocket(
- io
-){
+let io;
+
+export const initializeSocket =
+(server)=>{
+
+ io = new Server(
+  server,
+  {
+   cors:{
+    origin:"*",
+    methods:[
+     "GET",
+     "POST"
+    ]
+   }
+  }
+ );
 
  io.on(
   "connection",
-  socket=>{
+  (socket)=>{
 
-   registerCollaboration(
+   console.log(
+    "Socket Connected:",
+    socket.id
+   );
+
+   registerBuilderEvents(
     io,
     socket
    );
@@ -83,12 +52,34 @@ export default function initSocket(
     socket
    );
 
+   registerCollaboration(
+    io,
+    socket
+   );
+
    registerComments(
     io,
     socket
    );
 
+   socket.on(
+    "disconnect",
+    ()=>{
+
+     console.log(
+      "Socket Disconnected:",
+      socket.id
+     );
+
+    }
+   );
+
   }
  );
 
-}
+ return io;
+
+};
+
+export const getIO =
+()=>io;
