d61b3153e87164864064f404acbca47863847ce2
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-19 23:58:55 +0530

    cli

--- admin/src/components/Generate.tsx
+++ admin/src/components/Generate.tsx
@@ -651,4 +651,63 @@ export default function Genrate({
             {/* Dots */}
             <div className="flex justify-center gap-2 mt-8">
                             <button key={i} onClick={() => setActiveTestimonial(i)}
-                className=
+                className=export default function ActivityFeed({
+ activities
+}){
+
+ return(
+
+ <div
+ className="
+ bg-white
+ border
+ rounded-xl
+ p-5
+ "
+ >
+
+  <h2
+   className="
+   font-semibold
+   mb-4
+   "
+  >
+   Activity Feed
+  </h2>
+
+  <div
+   className="
+   space-y-3
+   "
+  >
+
+   {activities.map(
+    activity=>(
+
+     <div
+      key={
+       activity._id
+      }
+
+      className="
+      border-b
+      pb-2
+      "
+     >
+
+      {
+       activity.message
+      }
+
+     </div>
+
+    )
+   )}
+
+  </div>
+
+ </div>
+
+ );
+
+}
