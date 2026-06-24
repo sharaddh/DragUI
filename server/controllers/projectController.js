5b2befe2571c1ab58a72c80d53598108ee1bb1ec
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-24 19:51:43 +0530

    feat: add project CRUD controller methods

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,68 +1,58 @@
-import * as projectService
-from "../services/projectService.js";
+import * as projectService from "../services/projectService.js";
 
-export const create =
-async (
- req,
- res
-) => {
-
- try {
-
-   const project =
-     await projectService.createProject(
-       req.body,
-       req.userId
-     );
-
-   res.status(201).json({
-     success:true,
-     project
-   });
-
- } catch(error){
-
-   res.status(500).json({
-     success:false,
-     message:error.message
-   });
-
- }
-
+export const create = async (req, res) => {
+  try {
+    const project = await projectService.createProject(req.body, req.userId);
+    res.status(201).json({ success: true, project });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
-export const getOne =
-async (
- req,
- res
-) => {
+export const getOne = async (req, res) => {
+  try {
+    const project = await projectService.getProject(req.params.projectId);
+    if (!project) {
+      return res.status(404).json({ success: false });
+    }
+    res.json({ success: true, project });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
- try {
+export const list = async (req, res) => {
+  try {
+    const projects = await projectService.listProjects(req.userId);
+    res.json({ success: true, projects });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-   const project =
-     await projectService.getProject(
-       req.params.projectId
-     );
+export const save = async (req, res) => {
+  try {
+    const project = await projectService.saveProject(req.userId, req.body);
+    res.json({ success: true, project });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-   if(!project){
-     return res.status(404)
-       .json({
-         success:false
-       });
-   }
+export const deleteProject = async (req, res) => {
+  try {
+    await projectService.deleteProject(req.userId, req.params.projectId);
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-   res.json({
-     success:true,
-     project
-   });
-
- } catch(error){
-
-   res.status(500).json({
-     success:false,
-     message:error.message
-   });
-
- }
-
+export const update = async (req, res) => {
+  try {
+    const project = await projectService.updateProject(req.userId, req.params.projectId, req.body);
+    res.json({ success: true, project });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
