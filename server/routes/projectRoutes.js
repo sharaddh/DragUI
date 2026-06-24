f95026b95eabc40f906b14d4d12c2006a3b93c9d
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-24 19:52:00 +0530

    feat: add full project CRUD routes

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,42 +1,23 @@
 import express from "express";
+import authMiddleware from "../middleware/auth.middleware.js";
+import Project from "../models/Project.js";
+import * as projectController from "../controllers/projectController.js";
 
-import authMiddleware
-from "../middleware/auth.middleware.js";
+const router = express.Router();
 
-import * as projectController
-from "../controllers/projectController.js";
+router.get("/marketplace", async (req, res) => {
+  const projects = await Project.find({
+    isMarketplace: true,
+    visibility: "public",
+  });
+  res.json({ success: true, projects });
+});
 
-const router =
- express.Router();
-router.get(
- "/marketplace",
- async (
-   req,
-   res
- ) => {
-
-   const projects =
-     await Project.find({
-       isMarketplace:true,
-       visibility:"public"
-     });
-
-   res.json({
-     success:true,
-     projects
-   });
-
- }
-);
-router.post(
- "/",
- authMiddleware,
- projectController.create
-);
-
-router.get(
- "/:projectId",
- projectController.getOne
-);
+router.post("/", authMiddleware, projectController.create);
+router.get("/list", authMiddleware, projectController.list);
+router.post("/save", authMiddleware, projectController.save);
+router.get("/:projectId", projectController.getOne);
+router.put("/:projectId", authMiddleware, projectController.update);
+router.delete("/:projectId", authMiddleware, projectController.deleteProject);
 
 export default router;
